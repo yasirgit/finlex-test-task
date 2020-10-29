20f3cc51f74456333a35596eb8b2bff6d70e499e
Yasir Arefin <sobujarefin@gmail.com>
2020-10-29 18:50:05 +0100

    added unit test shell

--- robotcontrollerapi/config/db.js
+++ robotcontrollerapi/config/db.js
@@ -1,31 +1,11 @@
 const mysql = require("mysql");
-require('dotenv/config');
 
-
-// console.log(process.env);
-
-// Create a connection to the database
 const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'robot-position-db'
+    host: 'kavfu5f7pido12mr.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+    user: 's47a441lnzzacrha',
+    password: 'knni0klsmbhgr2ru',
+    database: 'rei7ydl0f2fmwa70'
 });
-
-// const connection = mysql.createConnection({
-//     host: process.env.DB_HOST,
-//     user: process.env.DB_USER,
-//     password: process.env.DB_PASSWORD,
-//     database: process.env.DB_NAME
-// });
-
-
-// const connection = mysql.createConnection({
-//     host: 'kavfu5f7pido12mr.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
-//     user: 's47a441lnzzacrha',
-//     password: 'knni0klsmbhgr2ru',
-//     database: 'rei7ydl0f2fmwa70'
-// });
 
 // open the MySQL connection
 connection.connect(error => {
