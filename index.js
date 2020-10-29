22b8017cc6da172d3ef4756a5ab914473ef3f9cf
Yasir Arefin <sobujarefin@gmail.com>
2020-10-29 11:49:10 +0100

    deploy to heroku

--- index.js
+++ index.js
@@ -12,6 +12,8 @@ app.get('/', (req, res) => {
 });
 
 // set port, listen for requests
-app.listen(5000, () => {
-    console.log("Server is running on port 5000.");
-});
+// app.listen(5000, () => {
+//     console.log("Server is running on port 5000.");
+// });
+
+app.listen(process.env.PORT || 5000);
