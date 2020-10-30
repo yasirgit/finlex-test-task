99edd3fc408d9e18d5799e70f2105c9fda769366
Yasir Arefin <sobujarefin@gmail.com>
2020-10-30 08:31:47 +0100

    added api doc

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 
+const swaggerUi = require('swagger-ui-express'),
+    swaggerDocument = require('./swagger.json');
+
 app.use(bodyParser.json());
 
 require("./robotcontrollerapi/routes/robotcommand.routes")(app);
@@ -12,6 +15,8 @@ app.get('/', (req, res) => {
 });
 
 const port = process.env.PORT || 5000;
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 // set port, listen for requests
 app.listen(port, () => {
