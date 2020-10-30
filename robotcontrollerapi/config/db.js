e3e855ea411e7cbc780c8c6c0d6d328cf22e0b50
Yasir Arefin <sobujarefin@gmail.com>
2020-10-30 13:16:49 +0100

    added comment docs

--- robotcontrollerapi/config/db.js
+++ robotcontrollerapi/config/db.js
@@ -2,11 +2,20 @@ const mysql = require("mysql");
 require('dotenv').config();
 
 const connection = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
+    host: 'kavfu5f7pido12mr.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+    user: 's47a441lnzzacrha',
+    password: 'knni0klsmbhgr2ru',
+    database: 'rei7ydl0f2fmwa70'
 });
+
+// mysql://s47a441lnzzacrha:knni0klsmbhgr2ru@kavfu5f7pido12mr.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/rei7ydl0f2fmwa70
+
+// const connection = mysql.createConnection({
+//     host: process.env.DB_HOST,
+//     user: process.env.DB_USER,
+//     password: process.env.DB_PASSWORD,
+//     database: process.env.DB_NAME
+// });
 
 // open the MySQL connection
 connection.connect(error => {
